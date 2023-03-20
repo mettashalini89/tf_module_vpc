a9a2563d7a707de40328dcb8cae2e5e5a191b651
mettashalini89@gmail.com <CatMatDog@123>
2023-03-20 13:52:48 -0600

    public subnets creation

--- main.tf
+++ main.tf
@@ -5,3 +5,16 @@ resource "aws_vpc" "vpc" {
     {Name = "${var.env}-vpc"}
   )
 }
+
+## Public subnets
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.vpc.id
+  tags = merge(
+    var.tags,
+    {Name = "${var.env}-${each.value["name"]}"}
+  )
+  for_each = var.public_subnets
+  cidr_block = each.value["cidr_block"]
+  availability_zone = each.value["availability_zone"]
+
+}
