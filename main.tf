bed0d38671d5e44e72a7762fb47fb4e23d2be2f9
mettashalini89@gmail.com <CatMatDog@123>
2023-03-20 14:28:32 -0600

    Route table creation

--- main.tf
+++ main.tf
@@ -19,6 +19,17 @@ resource "aws_subnet" "public_subnets" {
 
 }
 
+## Public route table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  for_each = var.public_subnets
+  tags = merge(
+    var.tags,
+    {Name = "${var.env}-${each.value["name"]}"}
+  )
+}
+
 ####private subnets
 resource "aws_subnet" "private_subnets" {
   vpc_id = aws_vpc.vpc.id
@@ -30,4 +41,15 @@ resource "aws_subnet" "private_subnets" {
   cidr_block = each.value["cidr_block"]
   availability_zone = each.value["availability_zone"]
 
+}
+
+####private route table
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  for_each = var.private_subnets
+  tags = merge(
+    var.tags,
+    {Name = "${var.env}-${each.value["name"]}"}
+  )
 }
